0c1cbcaf21dd11eeccacf6da5a81d3411169fbad
Jose Contreras <jocontre@joselcontreras.com>
2013-11-04 13:13:38 -0600

    Create leap_year.rb

---
+++ leap_year.rb
@@ -0,0 +1,12 @@
+puts "Enter an initial year in format ####"
+initial = gets.chomp
+puts "Now enter end year in format ####"
+ends = gets.chomp
+leap = []
+while initial.to_i <= ends.to_i
+if initial.to_i % 4 == 0 && num.to_i % 400 == 0)
+leap << num
+num += 1
+end 
+puts leap
+end
