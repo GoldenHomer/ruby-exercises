a9bf0bd84101ca5eace3722d0c25c2b0f0b10e08
Jose Contreras <jocontre@joselcontreras.com>
2014-07-29 15:51:20 -0700

    This is different

--- backAtIt/guessing_game.rb
+++ backAtIt/guessing_game.rb
@@ -1,11 +1,20 @@
-puts "Guessing Game!"
-puts "-"*20
-print "Guess my number > "
-number = 5
-guess = get.to_i
+number = rand(10) + 1
 
-if guess == number
-  puts "You win."
+puts "Guessing Game!"
+puts "-" * 20
+
+5.times do
+	print "Guess my number (1-10) > "
+	# print will display the string in its own line
+	guess = gets.to_i
+	if guess == number
+		won = true
+		break
+	end
+end
+
+if won
+	puts "You win."
 else 
-  puts "You lost. The number was " + number.to_s
+	puts "You lost. The number was #{number}"
 end
