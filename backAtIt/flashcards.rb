c597153b93257937b350f8e09aea4b16af3ba119
Jose Contreras <jocontre@joselcontreras.com>
2014-07-30 16:09:37 -0700

    More on Ruby

--- backAtIt/flashcards.rb
+++ backAtIt/flashcards.rb
@@ -1,4 +1,25 @@
-card = "cat|neko"
-card2 = "dog|inu"
-deck = [card, card2, 187,[]]
-puts deck
+card = {
+	"front" => "cat",
+	"back" => "neko"
+}
+
+card2 = {
+	"front" => "dog",
+	"back" => "inu"
+}
+
+deck = [card, card2]
+
+deck.each do |i|
+	front = card["front"]
+	back = card["back"]
+
+	puts "#{front} > "
+	guess = gets.chomp # Remember, chomp is a method that removes the return character at the end.
+
+	if guess = back
+		puts "Correct"
+	else
+		puts "Incorrect. The answer was #{back}"
+	end
+end
