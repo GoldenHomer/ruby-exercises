535c25fc16643faaddcf9767798cef94bb8b96cf
Jose Contreras <jocontre@joselcontreras.com>
2014-07-30 16:54:40 -0700

    Refactoring

--- backAtIt/flashcards.rb
+++ backAtIt/flashcards.rb
@@ -1,18 +1,54 @@
-card = {
-	"front" => "cat",
-	"back" => "neko"
-}
+# Capital letter for class name is necessary!
+class Card
+	# When you need both a reader and writer for an instance variable, you can shorthand these as:
+	attr_accessor :front, :back
 
-card2 = {
-	"front" => "dog",
-	"back" => "inu"
-}
+	# Paretheses are optional in Ruby
+	def initialize front, back
+		@front = front
+		@back = back
+		# Like the keyword this in JavaScript when creating an constructor
+	end
 
-deck = [card, card2]
+	# def front
+	# 	@front
+	# end
 
-deck.each do |i|
-	front = card["front"]
-	back = card["back"]
+	# Below is shorthand for above (for both parameters)
+		#attr_reader :front, :back
+
+	# def front= front
+	# 	@front = front
+	# end
+
+	# Same here as well
+		#attr_writer :front, :back
+
+	# Both of these are typical placed on the first line after class
+end
+
+class Deck
+	def initialize name
+		@name = name
+		@cards = []
+	end
+
+	def add_card card
+		@cards << card
+	end
+end
+
+card1 = Card.new("cat","neko")
+card2 = Card.new("dog","inu")
+
+deck = Deck.new("Japanese")
+
+deck.add_card card1
+deck.add_card card2
+
+deck.cards.each do |card|
+	front = card.front
+	back = card.back
 
 	puts "#{front} > "
 	guess = gets.chomp # Remember, chomp is a method that removes the return character at the end.
